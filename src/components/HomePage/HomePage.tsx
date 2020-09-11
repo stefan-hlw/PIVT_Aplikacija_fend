9b7af04c2f28457e24de5c5c5240414254e00c15
Stefan Vucan <stefan.vucan.16@singimail.rs>
2020-09-11 17:39:15 +0200

    Minor routing fix

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,15 +1,99 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { Container, Card, Row, Col } from 'react-bootstrap';
+import { faListAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CategoryType from '../../types/CategoryType';
+import { Link } from 'react-router-dom';
+import api, { ApiResponse } from '../../api/api';
 
-function HomePage() {
-  return (
-    <Container>
-      <FontAwesomeIcon icon={ faHome } />
-      Home
-    </Container>
+interface HomePageState {
+  isUserLoggedIn: boolean;
+  categories: CategoryType[];
+}
+
+interface ApiCategoryDto {
+  categoryId: number;
+  name: string;
+}
+
+class HomePage extends React.Component {
+  state: HomePageState;
+
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.state = {
+      isUserLoggedIn: true,
+      categories: [],
+    } 
+  }
+
+  componentWillMount() {
+    this.getCategories();
+  }
+
+  componentWillUpdate() {
+    this.getCategories();
+  }
+
+  private getCategories() {
+    api('category/', 'get', {})
+    .then((res: ApiResponse) => {
+        this.putCategoriesInState(res.data);
+    });
+  }
+
+  private putCategoriesInState(data: ApiCategoryDto[]) {
+    const categories: CategoryType[] = data.map(category => {
+      return {  
+        categoryId: category.categoryId,
+        name: category.name,
+        items: [],
+      };
+    });
+
+    const newState = Object.assign(this.state, {
+      categories: categories
+    });
+
+    this.setState(newState);
+  }
+
+
+  render() {
+     return (
+      <Container>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <FontAwesomeIcon icon= {faListAlt} /> Top level categories
+            </Card.Title>
+               { this.state.categories.map(this.singleCategory) }
+              <Row>
+               { this.state.categories.map(this.singleCategory) }
+              </Row>
+          </Card.Body>
+        </Card>
+      </Container>   
   );
+}
+
+private singleCategory(category: CategoryType) {
+  return (
+    <Col md= "3">
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            { category.name }
+          </Card.Title>
+          <Link to={ `/category/${ category.categoryId }`}
+              className="btn btn-primary">
+                Open category
+          </Link>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+}
 }
 
 export default HomePage;
